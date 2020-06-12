489aff23fe051b31f972c9c45ddf4cc31cd8ac2c
Emmanuel Delbar <edelbar.fr@gmail.com>
2020-06-12 16:42:21 +0200

    Implement newArticle and deleteArticle routes

--- server/controllers/article.js
+++ server/controllers/article.js
@@ -1,11 +1,7 @@
 require('mariadb')
 const pool = require('../db_connect')
 
-// Create article
-exports.create = (req, res) => {
-}
-
-// Show all articles
+// Render all articles
 exports.allArticles = async (req, res) => {
 // Connection to db which will return the list of all articles
   try {
@@ -14,6 +10,44 @@ exports.allArticles = async (req, res) => {
     res.status(201).send(rows)
     conn.end()
   } catch (error) {
+    console.log(error)
+  }
+}
+
+// Create a new article
+exports.newArticle = async (req, res) => {
+  try {
+    const { title, content, userId } = req.body
+    // Connection to Database
+    const conn = await pool.getConnection()
+
+    // Save new article inside database
+    await conn.query(
+      `INSERT INTO articles VALUES (NULL, '${title}', '${content}', NULL, '${userId}')`
+    )
+    res.status(201).send({ message: `Thanks for sharing your new article: ${title} !` })
+    conn.release()
+  } catch (error) {
+    res.status(500).json({ error })
+    console.log(error)
+  }
+}
+
+// Delete an article
+exports.deleteArticle = async (req, res) => {
+  try {
+    const id = req.params.id
+    // Connection to Database
+    const conn = await pool.getConnection()
+
+    // Save new article inside database
+    await conn.query(
+      `DELETE FROM articles WHERE id = '${id}'`
+    )
+    res.status(200).send({ message: 'Article deleted' })
+    conn.release()
+  } catch (error) {
+    res.status(500).json({ error })
     console.log(error)
   }
 }
--- server/routes/article.js
+++ server/routes/article.js
@@ -3,7 +3,8 @@ const router = express.Router()
 
 const articleController = require('../controllers/article.js')
 
-router.post('/', articleController.create)
+router.post('/', articleController.newArticle)
 router.get('/', articleController.allArticles)
+router.delete('/:id', articleController.deleteArticle)
 
 module.exports = router
