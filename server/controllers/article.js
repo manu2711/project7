3410b1747291456c01e2ac13fad8c6a5b1055cb1
Emmanuel Delbar <edelbar.fr@gmail.com>
2020-06-15 14:56:17 +0200

    implementation of oneArticle route/controller

--- server/controllers/article.js
+++ server/controllers/article.js
@@ -1,37 +1,64 @@
 require('mariadb')
 const pool = require('../db_connect')
 
-// Render all articles
-exports.allArticles = async (req, res) => {
-// Connection to db which will return the list of all articles
-  try {
-    const conn = await pool.getConnection()
-    const rows = await conn.query('SELECT * FROM articles')
-    res.status(201).send(rows)
-    conn.end()
-  } catch (error) {
-    console.log(error)
-  }
-}
-
 // Create a new article
 exports.newArticle = async (req, res) => {
   try {
     const { title, content, userId } = req.body
+    const url = Math.round(Math.random() * 1000000) + '-' + title.toLowerCase().split(' ').join('-')
     // Connection to Database
     const conn = await pool.getConnection()
 
     // Save new article inside database
     await conn.query(
-      `INSERT INTO articles VALUES (NULL, '${title}', '${content}', NULL, '${userId}')`
+      `INSERT INTO articles VALUES (NULL, "${title}", "${content}", NULL, "${userId}", '${url}')`
     )
-    res.status(201).send({ message: `Thanks for sharing your new article: ${title} !` })
+    res
+      .status(201)
+      .send({ message: `Thanks for sharing your new article: ${title} ! avec l'url: ${url}` })
     conn.release()
   } catch (error) {
     res.status(500).json({ error })
     console.log(error)
   }
 }
+
+// Render all articles
+exports.allArticles = async (req, res) => {
+  // Connection to db which will return the list of all articles
+  try {
+    const conn = await pool.getConnection()
+    const rows = await conn.query(
+      'SELECT * FROM articles ORDER BY article_date DESC'
+    )
+    res.status(200).send(rows)
+    conn.end()
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+// Render one article
+exports.oneArticle = async (req, res) => {
+  try {
+    const articleId = req.params.id
+
+    const conn = await pool.getConnection()
+    const rows = await conn.query(
+      `SELECT * FROM articles WHERE id='${articleId}'`
+    )
+
+    if (rows[0] == null) return res.send({ message: 'There is no article with that id !' })
+
+    res.status(200).send(rows)
+    conn.end()
+  } catch (error) {
+    res.status(500).json({ error })
+    console.log(error)
+  }
+}
+
+
 
 // Delete an article
 exports.deleteArticle = async (req, res) => {
@@ -41,9 +68,7 @@ exports.deleteArticle = async (req, res) => {
     const conn = await pool.getConnection()
 
     // Save new article inside database
-    await conn.query(
-      `DELETE FROM articles WHERE id = '${id}'`
-    )
+    await conn.query(`DELETE FROM articles WHERE id = '${id}'`)
     res.status(200).send({ message: 'Article deleted' })
     conn.release()
   } catch (error) {
--- server/routes/article.js
+++ server/routes/article.js
@@ -5,6 +5,7 @@ const articleController = require('../controllers/article.js')
 
 router.post('/', articleController.newArticle)
 router.get('/', articleController.allArticles)
+router.get('/:id', articleController.oneArticle)
 router.delete('/:id', articleController.deleteArticle)
 
 module.exports = router
