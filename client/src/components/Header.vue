3671c2e56cea9de70acbdabca71acc0dd1a87ccc
Emmanuel Delbar <edelbar.fr@gmail.com>
2020-06-16 14:43:33 +0200

    Update Header ul

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -8,12 +8,13 @@
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
           <b-nav-item to="/">Home</b-nav-item>
-          <b-nav-item to="/dashboard">Dashboard</b-nav-item>
+          <b-nav-item to="/compose">Compose</b-nav-item>
           <b-nav-item to="#">Admin</b-nav-item>
         </b-navbar-nav>
 
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
+          <b-nav-item v-if="isLoggedIn" href="#">{{this.$store.getters.userName}}</b-nav-item>
           <b-nav-item v-if="isLoggedIn" href="#" @click="logout">Logout</b-nav-item>
         </b-navbar-nav>
       </b-collapse>
