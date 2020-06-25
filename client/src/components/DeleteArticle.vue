64334131da1c9a7447289ee6a03ab4cfac022000
Emmanuel Delbar <edelbar.fr@gmail.com>
2020-06-25 19:05:09 +0200

    Creation of DeleteArticle componenet

---
+++ client/src/components/DeleteArticle.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <b-button
+            variant="danger"
+            @click="deleteArticle(articleId)"
+        >Delete</b-button>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+    export default {
+        name: 'DeleteArticle',
+        props: {
+            articleId: Number
+        },
+        methods: {
+            deleteArticle: async function (articleId) {
+                try {
+                    axios.delete(`http://localhost:3000/api/articles/${articleId}`)
+                    .then((response) => {
+                    console.log(response.data)
+                    this.$router.go()
+                    })
+                } catch (error) {
+                    console.log(error)
+                }
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
